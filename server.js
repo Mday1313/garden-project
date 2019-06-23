bd4fe61a989b75b388230e4b66853ba6b2438b8e
Mday1313 <melissaday076@gmail.com>
2019-06-23 16:11:07 -0400

    test commit

---
+++ server.js
@@ -0,0 +1,49 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = 3000;
+
+// Requiring the `User` model for accessing the `users` collection
+var User = require("./userModel.js");
+
+// Initialize Express
+var app = express();
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/tessel", { useNewUrlParser: true });
+
+// Routes
+
+app.post("/submit", function(req, res) {
+  
+
+  var user = new User(req.body);
+  user.coolifier();
+  user.makeCool();
+
+  User.create(user)
+    .then(function(dbUser) {
+      
+      res.json(dbUser);
+    })
+    .catch(function(err) {
+     
+      res.json(err);
+    });
+});
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
